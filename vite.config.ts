c35b080d983605853afbd2d92e586f74f0f9a46b
kiwizzard <doublesunking@gmail.com>
2026-01-22 16:54:55 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // 这一行非常关键：它告诉 Vite 在构建时将代码中的 process.env.API_KEY 替换为真实的值
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
